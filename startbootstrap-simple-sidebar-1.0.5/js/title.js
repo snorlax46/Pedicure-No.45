56cbd95dfe097e5db532542ad277f42198731e18
snorlax46 <snorlax46@users.noreply.github.com>
2016-03-11 19:05:42 +0100

    added javasript for title

---
+++ startbootstrap-simple-sidebar-1.0.5/js/title.js
@@ -0,0 +1,10 @@
+var title = `
+    <div class="row">
+        <div class="col-md-3">
+            <a href="index.html"><img src="images/pedicurelogo.gif" class="logo" alt="Logo"></a>
+        </div>
+        <div class="col-md-9">
+            <h1 class="title"><a href="index.html"><b>Pedicurestudio No.45</b></a></h1>
+        </div>
+    </div>`;
+document.write(title);
